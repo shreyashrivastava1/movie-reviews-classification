dddeb615d32ed70825dc5e49ea0725c34b247783
Shreya Shrivastava <shreya.shrivastava1003@gmail.com>
2025-02-24 03:19:18 +0530

    made more minor changes

--- src/components/BE_3.js
+++ src/components/BE_3.js
@@ -4,13 +4,13 @@ export default function BE_3() {
   const [stars, setStars] = useState([]);
 
   useEffect(() => {
-    const newStars = Array.from({ length: 500 }, (_, i) => ({
+    const newStars = Array.from({ length: 700 }, (_, i) => ({
       id: i,
       size: Math.random() * 3 + 1.5,
       top: `${Math.random() * 100}%`,
       left: `${Math.random() * 100}%`,
       animationDelay: `${Math.random() * 2}s`,
-      opacity: Math.random() * 0.08 + 0.02,
+      opacity: Math.random() * 0.2 + 0.1,
     }));
 
     setStars(newStars);
@@ -18,8 +18,6 @@ export default function BE_3() {
 
   return (
     <div className="fixed inset-0 bg-black overflow-hidden">
-      {/* <div className="background-glow"></div> */}
-
       {stars.map((star) => (
         <div
           key={star.id}
--- src/components/SubmitButton.js
+++ src/components/SubmitButton.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function SubmitButton() {
   return (
     <div className="flex justify-center mt-4">
-      <button className="px-6 py-2 text-lg font-semibold bg-pink-500 hover:bg-pink-600 text-white rounded-full shadow-md neon-button transition rounded">
+      <button className="px-6 py-2 text-lg font-semibold bg-pink-500 hover:bg-pink-600 text-white rounded-full shadow-md neon-button transition ">
         Analyze Review
       </button>
     </div>
